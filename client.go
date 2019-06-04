8615181abb14339a125859d6c8c183bfa0b46753
lanhuidong <lanhuidong@gmail.com>
2019-06-04 14:46:02 +0800

    支持h2c

---
+++ client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"golang.org/x/net/http2"
+	"crypto/tls"
+	"net"
+	"log"
+	"fmt"
+)
+
+func main() {
+	client := http.Client{
+		// Skip TLS dial
+		Transport: &http2.Transport{
+			AllowHTTP: true,
+			DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+				return net.Dial(network, addr)
+			},
+		},
+	}
+
+	resp, err := client.Get("http://localhost:8080")
+	if err != nil {
+		log.Fatal(fmt.Errorf("error making request: %v", err))
+	}
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Proto)
+}
---
+++ h2c.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+	"golang.org/x/net/http2"
+	"net"
+)
+
+type serverHandler struct {
+}
+
+func (sh *serverHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(req)
+	w.Header().Set("server", "h2test")
+	w.Write([]byte("http2 without tls"))
+}
+
+func main() {
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      &serverHandler{},
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+	s2 := &http2.Server{
+		IdleTimeout: 1 * time.Minute,
+	}
+	http2.ConfigureServer(server, s2)
+	l, _ := net.Listen("tcp", ":8080")
+	defer l.Close()
+	fmt.Println("Start server...")
+	for {
+		rwc, err := l.Accept()
+		if err != nil {
+			fmt.Println("accept err:", err)
+			continue
+		}
+		go s2.ServeConn(rwc, &http2.ServeConnOpts{BaseConfig: server})
+
+	}
+}
