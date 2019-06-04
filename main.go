9674b6b121a11e6c98027997c4caa8b81068040e
lanhuidong <lanhuidong@gmail.com>
2019-06-04 11:42:37 +0800

    add golang http2 demo

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, HTTP2!")
+}
+
+func main() {
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/", handler)
+	server := http.Server{
+		Addr:    "127.0.0.1:8443",
+		Handler: serverMux,
+	}
+	server.ListenAndServeTLS("server.crt", "server.key")
+}
